8354231d183b7483eafdbf5e8a3898417ad73935
Diego Calderon <124089036+romilan24@users.noreply.github.com>
2023-11-20 20:32:32 -0800

    Update config.py

--- config.py
+++ config.py
@@ -2,8 +2,8 @@ from datetime import date
 
 # Set Date format like: YYYY, MM, DD
 # Note that FIND_FIRST_DATE uses START_DATE as default start date
-START_DATE = date(1990, 1, 1)
-END_DATE = date(2020, 7, 19)
+START_DATE = date(2020, 1, 1)
+END_DATE = date(2023, 7, 31)
 
 # set to "metric" or "imperial"
 UNIT_SYSTEM = "metric"
